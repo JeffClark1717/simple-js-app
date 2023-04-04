ba174d268af0b019de1d42a5aef5805a341b7b7d
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-04-03 17:07:18 -0700

    Created Pokedex objects

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favouriteFood = 'MEAT';
-document.write(favouriteFood);
+pokemon[0] = {
+    name: "Charizard",
+    height: 10,
+    weight: 500,
+    type: ["fire", "fighting"]
+};
+
+pokemon[1] = {
+    name: "Geodude",
+    height: 3.2,
+    weight: 75,
+    type: ["fighting", "floating"]
+};
+
+pokemonList[2] = {
+    name: "Stunky",
+    height: 1.1,
+    weight: 9,
+    type: ["grass", "poison"]
+};
+
+console.log(pokemonList);
+
