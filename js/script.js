9280c79a457774697a10622c2899219e9458e272
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-04-04 12:19:12 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 let pokemonList = [];
 
-pokemon[0] = {
+pokemonList[0] = {
     name: "Charizard",
     height: 10,
     weight: 500,
     type: ["fire", "fighting"]
 };
 
-pokemon[1] = {
+pokemonList[1] = {
     name: "Geodude",
     height: 3.2,
     weight: 75,
     type: ["fighting", "floating"]
 };
 
-pokemon[2] = {
+pokemonList[2] = {
     name: "Stunky",
     height: 1.1,
     weight: 9,
