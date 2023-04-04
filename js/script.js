931af898bbb187dd1bdf60e6bd7ea1122f7764fd
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-04-04 11:58:20 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,9 +21,6 @@ pokemon[2] = {
     type: ["grass", "poison"]
 };
 
-
-let pokemonBig = "
-
 for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height >= 6 && pokemonList[i].weight >= 60)
 document,write (pokemon.name + " is a Big " pokemon.type + "pokemon.")
