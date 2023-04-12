c299198cd6107136b5dd23ba1a59b830400225d4
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-04-12 10:20:37 -0700

    cleaned up code and corrected iife

--- js/script.js
+++ js/script.js
@@ -1,29 +1,5 @@
-/* let pokemonList = [];
 
-const pokemonList = [
-    {name: "Charizard", height: 10, weight: 500, type: ["fire", "fighting"}, 
-    {name: "Geodude", height: 3.2, weight: 75, type: ["fighting", "floating"]}, 
-    {name: "Stunky", height: 1.1, weight: 9, type: ["grass", "poison"]}
-]
-
-console.log(pokemonList); 
-
-for (let i = 0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + "ft)" + " ");
-    if (pokemonList[i].height > 6){
-       document.write("- He's a big boy.");
-    } else {
-        document.write(" - He's a cute little fella.")
-    }
-    document.write("<br>")
-};
-
-*/
-
-let pokemonList = [];
-
-(function () {
-    let data = {};
+let pokemonRepository = (function () {
 
 let pokemonList = [
     {name: "Charizard", height: 10, weight: 500, type: ["fire", "fighting"]}, 
@@ -31,10 +7,19 @@ let pokemonList = [
     {name: "Stunky", height: 1.1, weight: 9, type: ["grass", "poison"]}
 ]
 
-console.log(pokemonList); 
+function getAll () {
+    return pokemonList;
+}
 
-pokemonList.forEach(function(name) {
-  document.write(name + " is " + height + " feet tall, " + weight + " pounds. And he is a " + type + "." );
-});
+function add (pokemon){
+    pokemonList(pokemon)
+}
 
-})();
+return {
+    getAll: getAll,
+    add: add
+}
+
+})()
+
+console.log(pokemonRepository.getAll())
